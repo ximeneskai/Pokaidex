7707c6c46d66867aa78cd72efeba931e9e9e800d
Kai Ximenes <kaiximenes@outlook.com>
2024-10-10 12:25:28 -0300

    Criação do modelo de Pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,30 @@
 
-const offset = 0;
-const limit = 10;
-const url =`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-fetch(url)
-.then((response) => response.json())
-.then((jsonBody) => console.log(jsonBody))
-.catch((error) => console.error(error))
+
+
+function convertPokemonTypesToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="pokemon-type">${typeSlot.type.name}</li>` )
+}
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon">
+            <span class="pokemon-number">${pokemon.order}</span>
+            <span class="pokemon-name">${pokemon.name}</span>
+            <div>
+                <ol class="pokemon-types">
+                    ${convertPokemonTypesToLi(pokemon.types).join('')}
+                </ol>
+                <img class="pokemon-img" src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemons-list')
+
+pokeApi.getPokemons().then((pokemons = []) => {
+    const newHtml = pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML = newHtml
+})
 
