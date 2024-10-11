09d532f044733f3e970ef4fe1ceaa4a908a15a31
Kai Ximenes <kaiximenes@outlook.com>
2024-10-11 12:07:42 -0300

    Página de Pokemon Detalhado - OK

--- assets/js/pokemon-details.js
+++ assets/js/pokemon-details.js
@@ -12,11 +12,27 @@ if (pokemonId) {
 
 function displayPokemonInfo(pokemon) {
     const pokemonInfoDiv = document.getElementById('pokemon-info');
+    pokemonInfoDiv.classList.remove(...pokemonInfoDiv.classList);
+    pokemonInfoDiv.classList.add(pokemon.type);
     pokemonInfoDiv.innerHTML = `
-    <h1>${pokemon.name}</h1>
-        <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
-        <p>Height: ${pokemon.height}</p>
-        <p>Weight: ${pokemon.weight}</p>
-        <p>Types: ${pokemon.types.map(typeInfo => typeInfo.type.name).join(', ')}</p>
-        <p>Abilities: ${pokemon.abilities.map(abilityInfo => abilityInfo.ability.name).join(', ')}</p>`;
+        <section class="pokemon-content-details">
+            <span class="pokemon-details-number">#${pokemon.id}</span>
+            <h1 class="pokemon-details-name">${pokemon.name}</h1>
+        </section>
+        <img class="pokemon-details-photo" src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
+        <section class="pokemon-details-infos">
+            <p class="pokemon-details-height">Altura: ${pokemon.height}</p>
+            <p class="pokemon-details-weight">Peso: ${pokemon.weight}</p>
+        </section>
+        <section class="pokemon-content-types">
+            <p class="pokemon-details-types">Tipo: ${pokemon.types.map(typeInfo => typeInfo.type.name).join(', ')}</p>
+        </section>
+        <section class="pokemon-content-skills">
+            <p class="pokemon-details-skills">Skills: ${pokemon.abilities.map(abilityInfo => abilityInfo.ability.name).join(', ')}</p>
+        </section>
+        <section class="pokemon-content-moves">
+            <p class="pokemon-details-moves">Movimentos: ${pokemon.moves.map(moveInfo => moveInfo.move.name).join(', ')}</p>
+        </section>
+    `;
 }
+
